b9804dc7dba1b6391bee4c485d1fcb0eae11c9aa
Luke Honiss <luke.honiss@gmail.com>
2019-08-21 21:37:25 +1200

    Add twitch embed to home page

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
+const config = require('config');
 const router = require('express').Router();
 
 const utils = require('../src/utils');
 
 router.get('/', (req, res) => {
-  return utils.render(req, res, 'index', 'Home', {});
+  return utils.render(req, res, 'index', 'Home', {channel: config.get('twitch.channel')});
 });
 
 module.exports = router;
