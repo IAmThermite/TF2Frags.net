15056ef70f0179d93b45d7dd5f6240e8ea380a6c
Luke Honiss <luke.honiss@gmail.com>
2019-08-26 15:18:13 +1200

    Add session secret option

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const store = new MongoDBStore({
 
 app.set('trust proxy', 1); // trust first proxy
 app.use(session({
-  secret: 'keyboard cat',
+  secret: config.get('app.sessionSecret'),
   resave: false,
   saveUninitialized: true,
   store,
